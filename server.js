b089e555b9503cf357e7e5a0fe7eabcd2aaaba0e
alias.co <bikramchatterjee@gmail.com>
2018-01-31 23:16:48 +0530

    update port in server.js in app listen

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
-// update port
 var PORT = process.env.PORT || 3000;
+
 var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
-app.get('/about',middleware.requireAuthentication,function(req,res){
-	res.send("About Us not really it is my story!");
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
 });
 
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000,function(){
-	console.log('Express Server Started on port '+PORT +'!');
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
